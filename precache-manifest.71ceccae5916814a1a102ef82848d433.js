ceee3539cbca0bcdecb0091e74227f506a5861b6
James Ravenscroft <hello@ravenscroftgames.net>
2020-01-02 11:32:11 -0800

    Updates

---
+++ precache-manifest.71ceccae5916814a1a102ef82848d433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe09c78b154109381b5c0e95b648ad4d",
+    "url": "/resume2020/index.html"
+  },
+  {
+    "revision": "9a1065417dc0d4fbd0e0",
+    "url": "/resume2020/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eae61b77e4bb4db13183",
+    "url": "/resume2020/static/js/2.739517c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resume2020/static/js/2.739517c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a1065417dc0d4fbd0e0",
+    "url": "/resume2020/static/js/main.1a1b76fc.chunk.js"
+  },
+  {
+    "revision": "872aeb45ab4f27aa786f",
+    "url": "/resume2020/static/js/runtime-main.f9397900.js"
+  }
+]);
